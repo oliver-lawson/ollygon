7433d9d17be7a8d0dbbc3dbd68d9336efd902902
Oliver Lawson <ollylawson@gmail.com>
2025-11-29 18:39:51 +0000

    [+] normals support for .obj import

--- src/core/io/import_mesh.cpp
+++ src/core/io/import_mesh.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <unordered_map>
 
 namespace ollygon {
 
@@ -17,183 +18,227 @@ MeshImportResult MeshImporter::import_obj(const std::string& filepath, Geo& out_
 
     std::cout << "Importing OBJ file: <" << filepath << std::endl;
 
+    // temporary storage - OBJ separates positions and normals
+    std::vector<Vec3> temp_positions;
+    std::vector<Vec3> temp_normals;
+
+    // map from (pos_idx, norm_idx) -> final vertex index in geo
+    std::unordered_map<VertexKey, uint32_t, VertexKeyHash> vertex_map;
+
     std::string line;
     int line_num = 0;
-    int vertex_count = 0;
-    int normal_count = 0;
-    int face_count = 0;
 
     while (std::getline(file, line)) {
-        if (!parse_obj_line(line, out_geo)) {
-            std::cerr << "Parse error at line " << line_num << ": " << line << std::endl;
-            return MeshImportResult::ParseError;
-        }
         line_num++;
+
+        // skip empty lines & comments
+        if (line.empty() || line[0] == '#') continue;
+
+        std::istringstream ss(line);
+        std::string prefix;
+        ss >> prefix;
+
+        if (prefix == "v") {
+            //vertex position: v x y z [w]
+            //eg: v 0.000000 2.000000 2.000000
+            //with optional [w] that defaults to 1.0
+            float x, y, z;
+            ss >> x >> y >> z;
+            if (ss.fail()) {
+                std::cerr << "Invalid vertex format at line " << line_num << std::endl;
+                return MeshImportResult::ParseError;
+            }
+            temp_positions.emplace_back(x, y, z);
+        }
+        else if (prefix == "vn") {
+            // vertex normal: vn x y z
+            float x, y, z;
+            ss >> x >> y >> z;
+            if (ss.fail()) {
+                std::cerr << "Invalid normal format at line " << line_num << std::endl;
+                return MeshImportResult::ParseError;
+            }
+            temp_normals.emplace_back(x, y, z);
+        }
+        else if (prefix == "vt") {
+            // texture coordinate: vt u v [w]
+            // texture coordinate - skip for now
+            continue;
+        }
+        else if (prefix == "f") {
+            // face
+            // f 1 2 3              (just vertex indices)
+            // f 1/1 2/2 3/3        (vertex/texture indices)
+            // f 1/1/1 2/2/2 3/3/3  (vertex/texture/normal indices)
+            // f 1//1 2//2 3//3     (vertex//normal indices, no texture)
+            //
+            //blender obj default seems to be v/vt/vn
+            std::vector<VertexKey> face_keys;
+            std::string vertex_data;
+
+            while (ss >> vertex_data) {
+                int v_idx = 0, vt_idx = 0, vn_idx = 0;
+
+                size_t first_slash = vertex_data.find('/');
+                if (first_slash == std::string::npos) {
+                    v_idx = std::stoi(vertex_data);
+                }
+                else {
+                    v_idx = std::stoi(vertex_data.substr(0, first_slash));
+                    size_t second_slash = vertex_data.find('/', first_slash + 1);
+
+                    if (second_slash == std::string::npos) {
+                        // v/vt
+                        if (first_slash + 1 < vertex_data.length()) {
+                            vt_idx = std::stoi(vertex_data.substr(first_slash + 1));
+                        }
+                    }
+                    else {
+                        // v/vt/vn or v//vn
+                        // 
+                        // check if texture coordinate between the slashes (v/vt/vn)
+                        // if slashes are adjacent (v//vn), this condition is false:
+                        if (second_slash > first_slash + 1) {
+                            vt_idx = std::stoi(vertex_data.substr(first_slash + 1, second_slash - first_slash - 1));
+                        }
+                        // parse normal index if present after the second slash
+                        if (second_slash + 1 < vertex_data.length()) {
+                            vn_idx = std::stoi(vertex_data.substr(second_slash + 1));
+                        }
+                    }
+                }
+
+                // convert to 0-indexed (OBJ is 1-indexed)
+                // also handle negative indices (relative to end)
+                if (v_idx > 0) v_idx -= 1;
+                else if (v_idx < 0) v_idx = temp_positions.size() + v_idx;
+
+                if (vn_idx > 0) vn_idx -= 1;
+                else if (vn_idx < 0) vn_idx = temp_normals.size() + vn_idx;
+                else vn_idx = -1;  // no normal specified
+
+                // validate
+                if (v_idx < 0 || v_idx >= (int)temp_positions.size()) {
+                    std::cerr << "Vertex index out of range at line " << line_num << std::endl;
+                    return MeshImportResult::ParseError;
+                }
+
+                face_keys.push_back({ v_idx, vn_idx });
+            }
+
+            if (face_keys.size() < 3) {
+                std::cerr << "Face has fewer than 3 vertices at line " << line_num << std::endl;
+                return MeshImportResult::ParseError;
+            }
+
+            // OBJ faces have 3+ sides - tris, quads, n-gons too.
+            // convert keys to actual vertex indices, creating new verts as needed
+            std::vector<uint32_t> face_indices;
+            for (const auto& key : face_keys) {
+                auto it = vertex_map.find(key);
+                if (it != vertex_map.end()) {
+                    face_indices.push_back(it->second);
+                }
+                else {
+                    // create new vertex
+                    Vec3 pos = temp_positions[key.pos_idx];
+                    Vec3 norm(0, 1, 0);  // default up if no normal
+
+                    if (key.norm_idx >= 0 && key.norm_idx < (int)temp_normals.size()) {
+                        norm = temp_normals[key.norm_idx];
+                    }
+
+                    uint32_t new_idx = out_geo.vertex_count();
+                    out_geo.add_vertex(pos, norm);
+                    vertex_map[key] = new_idx;
+                    face_indices.push_back(new_idx);
+                }
+            }
+
+            // triangulate (fan from first vertex)
+            //TODO: revisit once n-gon/quad first-class support
+            //TODO: add proper CDT or so once we have it,
+            // as this will break on concave polys
+            for (size_t i = 1; i < face_indices.size() - 1; i++) {
+                out_geo.add_tri(face_indices[0], face_indices[i], face_indices[i + 1]);
+            }
+        }
+        else if (prefix == "o" || prefix == "g" || prefix == "s" ||
+            prefix == "mtllib" || prefix == "usemtl") {
+            // object/group name: o name | g name
+            // smoothing group: s off | s 1 | s 2 ...
+            // material library / use material
+            // flattening everything into one mesh for now, so skip
+            //TODO: pass through obj not just geo
+            //TODO: material support
+            continue;
+        }
+        else {
+            // unknown line type - log it and don't fail
+            // probably lots more to add. until I exhaustively list them,
+            // i'm presuming the rest will work so lets pass true for now
+            std::cerr << "Skipping unknown OBJ line type: " << prefix << std::endl;
+        }
     }
-    std::cout << "OBJ import complete:" << std::endl;
+
+    // if no normals were in the file, compute them from faces
+    if (temp_normals.empty() && out_geo.tri_count() > 0) {
+        std::cout << "No normals in OBJ, computing face normals..." << std::endl;
+        compute_face_normals(out_geo);
+    }
 
     out_geo.source_file = filepath;
 
+    std::cout << "OBJ import complete: " << out_geo.vertex_count() << " vertices, "
+        << out_geo.tri_count() << " triangles" << std::endl;
+
     return MeshImportResult::Success;
-}
-
-bool MeshImporter::parse_obj_line(const std::string& line, Geo& geo)
-{
-    // skip empty lines & comments
-    if (line.empty() || line[0] == '#') { return true; }
-
-    std::istringstream ss(line);
-    std::string prefix;
-    ss >> prefix;
-
-    if (prefix == "v") {
-        //vertex position: v x y z [w]
-        //eg: v 0.000000 2.000000 2.000000
-        //with optional [w] that defaults to 1.0
-        float x, y, z;
-        ss >> x >> y >> z;
-
-        if (ss.fail()) {
-            std::cerr << "Invalid vertex format" << std::endl;
-            return false;
-        }
-
-        // store vertex with placeholder normal for now
-        // we'll fix normals later if the file has vn lines
-        geo.add_vertex(Vec3(x, y, z), Vec3(0, 0, 1));
-
-        return true;
-    }
-    if (prefix == "vn") {
-        // vertex normal: vn x y z
-        return true; //TODO
-    }
-    if (prefix == "vt") {
-        // texture coordinate: vt u v [w]
-        return true; //TODO
-    }
-    if (prefix == "f") { // face
-        // f 1 2 3              (just vertex indices)
-        // f 1/1 2/2 3/3        (vertex/texture indices)
-        // f 1/1/1 2/2/2 3/3/3  (vertex/texture/normal indices)
-        // f 1//1 2//2 3//3     (vertex//normal indices, no texture)
-
-        //blender obj default seems to be v/vt/vn
-
-        std::vector<int> face_verts;
-        std::string vertex_data;
-
-        while (ss >> vertex_data) {
-            // parse "v" or "v/vt" or "v/vt/vn" or "v//vn"
-            int v_idx = 0;
-            int vt_idx = 0;
-            int vn_idx = 0;
-
-            size_t first_slash = vertex_data.find('/'); //any slash?
-            if (first_slash == std::string::npos) { //non-position, not found
-                //first simplest forma, just f 1 2 3 indices
-                v_idx = std::stoi(vertex_data);
-            }
-            else {
-                // slashes
-                v_idx = std::stoi(vertex_data.substr(0, first_slash));
-
-                size_t second_slash = vertex_data.find('/', first_slash + 1);
-
-                if (second_slash == std::string::npos) {
-                    // format: v/vt
-                    if (first_slash + 1 < vertex_data.length()) {
-                        vt_idx = std::stoi(vertex_data.substr(first_slash + 1));
-                    }
-                }
-                else {
-                    // format: v/vt/vn or v//vn
-                    // 
-                    // check if texture coordinate between the slashes (v/vt/vn)
-                    // if slashes are adjacent (v//vn), this condition is false:
-                    if (second_slash > first_slash + 1) {
-                        vt_idx = std::stoi(vertex_data.substr(first_slash + 1, second_slash - first_slash - 1));
-                    }
-                    // parse normal index if present after the second slash
-                    if (second_slash + 1 < vertex_data.length()) {
-                        vn_idx = std::stoi(vertex_data.substr(second_slash + 1));
-                    }
-                }
-            }
-
-            if (v_idx > 0) {
-                v_idx -= 1; //obj indices are 1-indexed! convert
-            }
-            else if (v_idx < 0) {
-                //wrap negative indices
-                v_idx = geo.verts.size() + v_idx;
-            }
-
-            //validate index
-            if (v_idx < 0 || v_idx >= (int)geo.verts.size()) {
-                //shouldn't be here!
-                std::cerr << "Obj import: Vertex index out of range!: " << v_idx << std::endl;
-                return false;
-            }
-
-            face_verts.push_back(v_idx);
-            //TODO: handle vt_idxx and vn_idx similary once supported
-        } // END while (ss >> vertex_data){}
-
-        // OBJ faces have 3+ sides - tris,quads, n-gons too.
-        //triangulate if needed for now
-        //TODO: revisit once n-gon/quad first-class support
-        if (face_verts.size() < 3) {
-            std::cerr << "OBJ import: Face has few than 3 verts!" << std::endl;
-            return false;
-        }
-
-        if (face_verts.size() == 3) {
-            geo.add_tri(face_verts[0], face_verts[1], face_verts[2]);
-        }
-        else {
-            //triangulate from first vertex
-            //TODO add proper CDT or so once we have it,
-            // as this will break on concavepolys
-            for (size_t i = 1; i < face_verts.size() - 1; i++) {
-                geo.add_tri(face_verts[0], face_verts[i], face_verts[i+1]);
-            }
-        }
-
-        return true;
-    }
-   
-    if (prefix == "o" || prefix == "g") {
-        // object/group name: o name
-        // flattening everything into one mesh for now, so skip
-        //TODO -pass through obj not just geo
-        return true;
-    }
-
-    if (prefix == "s") {
-        // smoothing group: s off | s 1 | s 2 ...
-        //TODO
-        return true;
-    }
-
-    if (prefix == "mtllib" || prefix == "usemtl") {
-        // material library / use material
-        //TODO
-        return true;
-    }
-
-    // unknown line type - log it and don't fail
-    // probably lots more to add. until I exhaustively list them,
-    // i'm presuming the rest will work so lets pass true for now
-    std::cerr << "Skipping unknown OBJ line type: " << prefix << std::endl;
-    return true;
 }
 
 // == ply ==
 MeshImportResult MeshImporter::import_ply(const std::string& filepath, Geo& out_geo)
 {
     //TODO
-    return MeshImportResult();
+    return MeshImportResult::UnsupportedFormat;
+}
+
+void MeshImporter::compute_face_normals(Geo& geo)
+{
+    // reset all normals to zero
+    for (auto& v : geo.verts) {
+        v.normal = Vec3(0, 0, 0);
+    }
+
+    // accumulate face normals to vertices
+    for (size_t i = 0; i < geo.indices.size(); i += 3) {
+        uint32_t i0 = geo.indices[i];
+        uint32_t i1 = geo.indices[i + 1];
+        uint32_t i2 = geo.indices[i + 2];
+
+        Vec3 v0 = geo.verts[i0].position;
+        Vec3 v1 = geo.verts[i1].position;
+        Vec3 v2 = geo.verts[i2].position;
+
+        Vec3 edge1 = v1 - v0;
+        Vec3 edge2 = v2 - v0;
+        Vec3 face_normal = Vec3::cross(edge1, edge2);
+        //don't normalise yet - larger faces contribute more (area weighting)
+
+        geo.verts[i0].normal = geo.verts[i0].normal + face_normal;
+        geo.verts[i1].normal = geo.verts[i1].normal + face_normal;
+        geo.verts[i2].normal = geo.verts[i2].normal + face_normal;
+    }
+
+    // normalise all vertex normals
+    for (auto& v : geo.verts) {
+        float len = v.normal.length();
+        if (len > 1e-6f) {
+            v.normal = v.normal / len;
+        }
+        else {
+            v.normal = Vec3(0, 1, 0);  // fallback
+        }
+    }
 }
 
 } // namespace ollygon
--- src/core/io/import_mesh.hpp
+++ src/core/io/import_mesh.hpp
@@ -6,6 +6,22 @@
 
 namespace ollygon {
 
+// helper to hash a vertex/normal index pair for deduplication
+struct VertexKey {
+    int pos_idx;
+    int norm_idx;
+
+    bool operator==(const VertexKey& other) const {
+        return pos_idx == other.pos_idx && norm_idx == other.norm_idx;
+    }
+};
+
+struct VertexKeyHash {
+    size_t operator()(const VertexKey& k) const {
+        return std::hash<int>()(k.pos_idx) ^ (std::hash<int>()(k.norm_idx) << 16);
+    }
+};
+
 enum  class MeshImportResult {
     Success,
     FileNotFound,
@@ -19,7 +35,9 @@ public:
     static MeshImportResult import_ply(const std::string& filepath, Geo& out_geo);
 
 private:
-    static bool parse_obj_line(const std::string& line, Geo& geo);
+    // computes smooth vertex normals from face geometry
+    // (used when OBJ has no vn data)
+    static void compute_face_normals(Geo& geo);
 };
 
 } // namespace ollygon
